bd0926ce3cbf03093f3c362e003b456a1344feeb
victor <victorexposito@hotmail.dk>
2019-05-14 12:43:41 +0200

    Lavet Course model klasse

---
+++ src/main/java/kea/model/Course.java
@@ -0,0 +1,53 @@
+package kea.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    private int duration;
+    private String description;
+
+    public Course() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

a722ad949ecdc254f1ce3a05ed95298e169218a2
victor <victorexposito@hotmail.dk>
2019-05-17 11:51:39 +0200

    pls merge

--- src/main/java/kea/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package kea.repository;
-
-import kea.model.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer, Integer> {
-}

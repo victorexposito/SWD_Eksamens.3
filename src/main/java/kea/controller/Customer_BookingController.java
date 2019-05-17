1b460bf8b99d58677d5f8704659bfdd1de7f1e33
magnus <matroest@gmail.com>
2019-05-17 13:47:03 +0200

    t

---
+++ src/main/java/kea/controller/Customer_BookingController.java
@@ -0,0 +1,28 @@
+package kea.controller;
+
+import kea.repository.Customer_BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Customer_BookingController {
+    private final Customer_BookingRepository customer_bookingRepository;
+
+
+
+    @Autowired
+    public Customer_BookingController(Customer_BookingRepository customer_bookingRepository){
+        this.customer_bookingRepository = customer_bookingRepository;
+    }
+
+
+    @GetMapping("/adminmenu")
+    public String index(Model model){
+        model.addAttribute("customer_booking", customer_bookingRepository.readMenu());
+        return "adminmenu";
+    }
+
+
+}

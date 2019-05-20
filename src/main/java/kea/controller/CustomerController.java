c1639c5d56445af3f74d026624b745b9fe1d501a
victorexposito <victorexposito@hotmail.dk>
2019-05-20 10:55:18 +0200

    Lavet course menu- mangler stadig lidt arbejde

--- src/main/java/kea/controller/CustomerController.java
+++ src/main/java/kea/controller/CustomerController.java
@@ -1,9 +1,11 @@
 package kea.controller;
 
 
+import kea.repository.BookingRepository;
 import kea.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -13,8 +15,13 @@ public class CustomerController {
     @Autowired
     CustomerRepository CR;
 
+     @Autowired
+    BookingRepository BR;
+
     @PostMapping("/addBooking")
-    public String addBooking(){
+    public String addBooking(Model model){
+
+        model.addAttribute("customer_Booking",  )
         return null;
     }
 
